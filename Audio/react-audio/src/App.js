c1634eba029517bb989a5c109055255de0c5682c
MLavanya26 <lavanyamukka26@gmail.com>
2023-02-15 14:53:31 +0530

    final project submission

---
+++ Audio/react-audio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AudioRecorder from './AudioRecorder';
+
+function App() {
+  return (
+    <div className="App">
+     <AudioRecorder/>
+    </div>
+  );
+}
+
+export default App;
